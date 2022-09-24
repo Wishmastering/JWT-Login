100f528e54e6424cb700419f41e75f7dedc550bd
Wishmastering <charsdb@gmail.com>
2022-09-24 03:38:55 +0000

    token login R y logout R

--- src/main.py
+++ src/main.py
@@ -14,8 +14,11 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-#from models import Person
 
+
+
+
+#from models import Person
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -24,6 +27,11 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -44,7 +52,27 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    email_received = request.json.get("email", None)
+    password = request.json.get("password", None) 
+    
+    user_query = User.query.filter_by(email = email_received).first()
+    print(user_query.email)
+
+    if email_received != user_query.email or password != user_query.password :
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity= email_received)
+    return jsonify(access_token=access_token)
+
 # this only runs if `$ python src/main.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
